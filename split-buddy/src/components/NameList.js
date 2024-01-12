996dfc45b0220e0dbd46a0ffb39ddda4b10b641e
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-13 00:33:24 +0100

    Create Name List Component
    Created Name List Component, basic styling (will probably later)

---
+++ split-buddy/src/components/NameList.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import './index.css';
+
+const NameList = () => {
+    const [names, setNames] = useState([]);
+    const [newName, setNewName] = useState('');
+
+    const addName = (event) => {
+        event.preventDefault();
+        setNames(names.concat(newName));
+        setNewName('');
+    };
+
+    return (
+        <div className="name-list-container">
+            <h2>Who pays the bill?</h2>
+            <form>
+                <label>
+                    Add Name:
+                    <input 
+                        type="text" 
+                        value={newName} 
+                        onChange={(event) => setNewName(event.target.value)} />
+                </label>
+                <button type="submit" onClick={addName}>Add</button>
+            </form>
+
+            <ul>
+                {names.map((name, index) => <li key={index}>{name}</li>)}
+            </ul>
+        </div>
+    )
+}
+
+export default NameList;
