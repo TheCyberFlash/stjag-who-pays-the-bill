0c9ec51af302fde55da194a575c237d8bf411816
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-13 00:36:37 +0100

    Create Loser Component
    Created Loser Component, basic styling (will probably later)

---
+++ split-buddy/src/components/Loser.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import "./index.css";
+
+const Loser = ({ names }) => {
+    const [loser, setLoser] = useState('');
+
+    const selectLoser = (event) => {
+        event.preventDefault();
+        const randomIndex = Math.floor(Math.random() * names.length);
+        setLoser(names[randomIndex]);
+    };
+
+    return (
+        <div className="loser-container">
+            <h2>And the loser is...</h2>
+            <button type="submit" onClick={selectLoser}>Select</button>
+            {loser && <p>{loser} pays the bill!</p>}
+        </div>
+    )
+};
+
+export default Loser;
