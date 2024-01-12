f5d7b081a432a4481e03e6391332c948a5fb2e39
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-13 00:26:18 +0100

    (Very) Basic Styling

--- split-buddy/src/App.js
+++ split-buddy/src/App.js
@@ -2,20 +2,8 @@ import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>..Hello World..</h1>
     </div>
   );
 }
