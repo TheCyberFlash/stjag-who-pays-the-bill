e2f36b82cc2bc47df151dadfdc9881965d70722b
Steven Jagurić <steven.jaguric@gmail.com>
2024-01-13 00:41:21 +0100

    import fix
    needed extra .

--- split-buddy/src/components/Loser.js
+++ split-buddy/src/components/Loser.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./index.css";
+import "../index.css";
 
 const Loser = ({ names }) => {
     const [loser, setLoser] = useState('');
--- split-buddy/src/components/NameList.js
+++ split-buddy/src/components/NameList.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './index.css';
+import '../index.css';
 
 const NameList = () => {
     const [names, setNames] = useState([]);
